962e0031c2738313c231af423ae04fe386ad06b2
DarynHolmes <DarynHolmes@gmail.com>
2015-10-06 18:41:11 +0100

    Added work

--- app/game.js
+++ app/game.js
@@ -1,1 +1,15 @@
 alert("yo");
+
+var rows = [];
+
+rows[0] = [false, false, false, false, false, false, false, false, false, false];
+rows[1] = [false, false, false, false, false, false, false, false, false, false];
+rows[2] = [false, false, false, false, false, false, false, false, false, false];
+rows[3] = [false, false, false, false, false, false, false, false, false, false];
+rows[4] = [false, false, false, false, false, false, false, false, false, false];
+rows[5] = [false, false, false, false, false, false, false, false, false, false];
+rows[6] = [false, false, false, false, false, false, false, false, false, false];
+rows[7] = [false, false, false, false, false, false, false, false, false, false];
+rows[8] = [false, false, false, false, false, false, false, false, false, false];
+rows[9] = [false, false, false, false, false, false, false, false, false, false];
+
