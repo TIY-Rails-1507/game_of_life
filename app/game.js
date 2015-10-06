d618034ce9a3ddd12a28859f0b1b0249a13560c8
DarynHolmes <DarynHolmes@gmail.com>
2015-10-06 21:39:22 +0100

    Added work

--- app/game.js
+++ app/game.js
@@ -6,26 +6,33 @@ $(document).ready (function() {
   // 1 draw live cell
   // 2 dont do anything
 
-  board[0] = [1, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[1] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[2] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[3] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[4] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[5] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[6] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[7] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[8] = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2];
-  board[9] = [1, 2, 2, 2, 2, 2, 2, 2, 2, 1];
+  // board[0] = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // board[1] = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+  // board[2] = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+  // board[3] = [0, 1, 0, 1, 0, 0, 0, 0, 0, 0];
+  // board[4] = [0, 1, 1, 1, 1, 0, 0, 0, 0, 0];
+  // board[5] = [0, 0, 0, 1, 1, 0, 0, 0, 0, 0];
+  // board[6] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // board[7] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // board[8] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  // board[9] = [1, 0, 0, 0, 0, 0, 0, 0, 0, 1];
 
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
+  var size = 50;
 
+  board = create_board(size);
 
+  setInterval(function() { 
+    console.log("Drawing");
+    board = draw(board, ctx, size); 
+  }, 1000);
 
   
 
-  draw(board, ctx);
 
+
+  // alert("Done");
 
 });
 
@@ -39,17 +46,39 @@ function log(board, ctx) {
   }
 }
 
-function draw(board, ctx) {
+function create_board(size) {
 
-  var width = 50;
-  var height = width;
+ 
   var gridWidth = 500;
   var gridHeight = 500;
+  var width = gridHeight / size;
+  var height = width;
+  
+  var board = [];
+
+
+  for(var r = 0; r < size; r++) {
+      board[r] = [];
+      for(var c = 0; c < size; c++) {
+        board[r][c] = (c % 2 == 0 ? 1 : 0);
+      }
+  }
+  return board;
+}
+
+function draw(board, ctx, size) {
+
+  var gridWidth = 500;
+  var gridHeight = 500;
+  var width = gridHeight / size;
+  var height = width;
+
+  var next_board = [];
 
   ctx.fillRect(gridWidth-width,gridHeight-height,width,height);
 
   for(var r = 0; r < board.length; r++) {
-      
+      next_board[r] = [];
       for(var c = 0; c < board.length; c++) {
 
             var x = c * width;
@@ -61,12 +90,75 @@ function draw(board, ctx) {
 
             } else if (board[r][c] == 0){ // dead
               
-              ctx.fillStyle = "#FFFFFF";
+              ctx.fillStyle = "#BBFFFF";
               ctx.fillRect(x,y,width,height);
 
+            }  
+
+            // figure next value
+            var neighbourCount = countNeighbours(r,c,board); 
+
+            if (board[r][c] == 1) {
+
+              if (neighbourCount < 2) {
+                next_board[r][c] = 0;
+
+              } else if (neighbourCount > 3) { 
+
+                next_board[r][c] = 0;
+
+              } else {
+                next_board[r][c] = 1;
+              }
+            } else {
+              if (neighbourCount == 3) {
+
+                next_board[r][c] = 1;
+              } else {
+                next_board[r][c] = 0;
+              }
             }
-            
+
+
+
         }
   }
+  return next_board;
 }
+
+function countNeighbours(cell_row, cell_col, board) {
+  var count = 0;
+  if ( board[cell_row][cell_col+1] == 1) {
+    count++;
+  }
+  if ( board[cell_row][cell_col-1] == 1) {
+    count++;
+  }
+  if (board[cell_row + 1]) {
+    if (board[cell_row + 1][cell_col] == 1) {
+      count++;
+    }
+    if (board[cell_row + 1][cell_col - 1] == 1) {
+      count++;
+    }
+    if (board[cell_row + 1][cell_col + 1] == 1) {
+      count++;
+    }
+  }
+  if(board[cell_row - 1]) {
+    if (board[cell_row - 1][cell_col] == 1) {
+      count++;
+    }
+    if (board[cell_row - 1][cell_col - 1] == 1) {
+      count++;
+    }
+    if (board[cell_row - 1][cell_col + 1] == 1) {
+      count++;
+    }
+  }
+
+  return count;
+
+}
+
 
